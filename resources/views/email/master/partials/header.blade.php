5782591b2f41c0a7555c514c98b20ef8d6692a51
Ardeshir Gooran <ardeshirgooran@gmail.com>
2022-02-13 17:16:43 +0330

    added email master view

---
+++ resources/views/email/master/partials/header.blade.php
@@ -0,0 +1,9 @@
+<!-- ======= Header ======= -->
+<header id="header">
+    <div class="container d-flex align-items-center justify-content-between">
+        <h1 class="logo">
+            <img src="{{asset('img/logo.svg')}}" alt="Texon Logo">
+            <a href="{{url('/')}}">Texon</a>
+        </h1>
+    </div>
+</header><!-- End Header -->
