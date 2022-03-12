f02ffefe6ee489bccc8f74a38596b15c48237559
armin <master.armin@yahoo.com>
2022-03-12 15:55:18 +0330

    improved fron-end visuals

--- resources/views/project/index/section_projects_summary.blade.php
+++ resources/views/project/index/section_projects_summary.blade.php
@@ -1,21 +1,14 @@
-<style>
-    video{
-        width: 50% !important;
-        height: auto !important;
-        aspect-ratio: 1 / 1;
-    }
-</style>
 @foreach (json_decode($projects) as $project)
-    <div class="mb-5 bg-light bg-gradient container d-flex flex-column justify-content-center align-items-center position-relative" data-aos="zoom-out">
+    <div class="rounded shadow mb-5 bg-light bg-gradient container d-flex flex-column align-items-center position-relative" data-aos="zoom-out">
         <div class="row p-3">
             <video controls class="col" preload='none' poster={{"storage/"
                 . $project->poster}}>
                 <source src="{{ "storage/"
                     . json_decode($project->video)[0]->download_link }}" type="video/mp4">
             </video>
-            <div class="d-flex flex-column justify-content-between col">
+            <div class="d-flex flex-column justify-content-around col">
                 <h2 class="text-capitalize fs-1 p-2">{{$project->title}}</h2>
-                <h4 class="p-2 overflow-auto">{{$project->details}}</h4>
+                <h5 class="p-2 overflow-auto">{{$project->details}}</h4>
                 <a href="{{$project->website}}"
                     class="btn btn-primary p-2">{{$project->web_site_title}}</a>
             </div>
